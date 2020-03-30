07c5c5d7ee0d30b6981a1ed2b2d1b52c542531cd
Olaf Hering <olaf@aepfle.de>
2020-03-30 17:12:35 +0200

    specify a tftp server
    grub2 can not do proxy-DHCP...
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- config/ER-X/redo-erx.sh
+++ config/ER-X/redo-erx.sh
@@ -6,12 +6,14 @@ config_sh="${dir}/erx-config.vbash"
 vm_sh="${dir}/VMs.sh"
 phys="${dir}/physhosts.txt"
 password="$1"
+tftp_server="$2"
 test -n "${password}"
+test -n "${tftp_server}"
 test -f "${config_sh}"
 test -f "${vm_sh}"
 test -f "${phys}"
 pushd /tmp
 rm -f 'vm_static_host.txt'
 /bin/bash "${vm_sh}" --erx > 'vm_static_host.txt'
-time nohup vbash "${config_sh}" "${password}" "${phys}" 'vm_static_host.txt'
+time nohup vbash "${config_sh}" "${password}" "${phys}" 'vm_static_host.txt' "${tftp_server}"
 rm -f 'vm_static_host.txt'
